a099ce1d400fba677cb8b5e91e1b8309232b13f5
Dan McKinney <3dhdaniel@gmail.com>
2019-06-22 14:18:20 -0400

    second commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys figure out what set to return
+
+if(process.env.NODE_ENV === 'production') {
+// prod
+    module.exports = require('./prod');
+} else {
+// dev
+    module.exports = require('./dev');
+}
