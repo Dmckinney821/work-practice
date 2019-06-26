8ac060d261a1ae4e3cdac4f850f82953af3485bd
Dan McKinney <3dhdaniel@gmail.com>
2019-06-25 21:33:40 -0400

    third

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,6 +21,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'produciton') {
+    //Express will serve up production assests like main
+    app.use(express.static('client/build'));
+    //express will serve index.html if doens't recognize route
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
